38d77cc9e6608009a11a116f79f5e52938e3a51b
Chris Hupman <chupman@us.ibm.com>
2019-03-05 22:05:17 -0800

    Created new JanusGraph service wrapper and unit file. Now runs as ubnuntu user instead of root
    Signed-off-by: Chris Hupman <chupman@us.ibm.com>

--- reference_cluster/ansible/files/janusgraph-server.sh
+++ reference_cluster/ansible/files/janusgraph-server.sh
@@ -12,9 +12,6 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-#
-# would fix https://github.com/JanusGraph/janusgraph/issues/845
-
 
 ### BEGIN INIT INFO
 # Provides:          janusgraph-server
@@ -29,80 +26,94 @@
 
 [[ -n "$DEBUG" ]] && set -x
 
-SOURCE="$0"
+SOURCE="${BASH_SOURCE[0]}"
 while [[ -h "$SOURCE" ]]; do
-  cd -P "$( dirname "$SOURCE" )" || exit 1
-  DIR="$(pwd)"
+  BIN="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
   SOURCE="$(readlink "$SOURCE")"
-  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE"
+  [[ $SOURCE != /* ]] && SOURCE="$BIN/$SOURCE"
 done
+BIN="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 cd -P "$( dirname "$SOURCE" )" || exit 1
-GREMLIN_BIN="$(pwd)"
+# Set $CFG to $BIN/../conf/gremlin-server
+cd -P $BIN/../conf/gremlin-server
+CFG=$(pwd)
+# Set $LIB to $BIN/../lib
+cd -P $BIN/../lib
+LIB=$(pwd)
+# Set $LIB to $BIN/../ext
+cd -P $BIN/../ext
+EXT=$(pwd)
+# Initialize classpath to $CFG
+CP="$CFG"
+# Add the slf4j-log4j12 binding
+CP="$CP":$(find -L $LIB -name 'slf4j-log4j12*.jar' | sort | tr '\n' ':')
+# Add the jars in $BIN/../lib that start with "janusgraph"
+CP="$CP":$(find -L $LIB -name 'janusgraph*.jar' | sort | tr '\n' ':')
+# Add the remaining jars in $BIN/../lib.
+CP="$CP":$(find -L $LIB -name '*.jar' \
+                \! -name 'janusgraph*' \
+                \! -name 'slf4j-log4j12*.jar' | sort | tr '\n' ':')
+# Add the jars in $BIN/../ext (at any subdirectory depth)
+CP="$CP":$(find -L $EXT -name '*.jar' | sort | tr '\n' ':')
 
-GREMLIN_CONF=$GREMLIN_BIN/gremlin-server.conf
+JANUSGRAPH_SERVER_CMD=org.apache.tinkerpop.gremlin.server.GremlinServer
 
-[[ -r $GREMLIN_CONF ]] && source $GREMLIN_CONF
+# (Cygwin only) Use ; classpath separator and reformat paths for Windows ("C:\foo")
+[[ $(uname) = CYGWIN* ]] && CP="$(cygpath -p -w "$CP")"
+
+export CLASSPATH="${CLASSPATH:-}:$CP"
+
+# Change to $BIN's parent
+cd $BIN/..
+
 [[ -n "$DEBUG" ]] && set -x
 
-if [[ -z "$GREMLIN_HOME" ]]; then
-  cd ..
-  GREMLIN_HOME="$(pwd)"
+if [[ -z "$JANUSGRAPH_HOME" ]]; then
+  JANUSGRAPH_HOME="$(pwd)"
 fi
 
-if [[ -z "$LOG_DIR" ]] ; then
-  LOG_DIR="$GREMLIN_HOME/logs"
+if [[ -z "$LOG4J_CONF" ]]; then
+  LOG4J_CONF="file:$JANUSGRAPH_HOME/conf/gremlin-server/log4j-server.properties"
 fi
 
-if [[ -z "$LOG_FILE" ]]; then
-  LOG_FILE="$LOG_DIR/gremlin.log"
+if [[ -z "$JANUSGRAPH_LOGDIR" ]] ; then
+  JANUSGRAPH_LOGDIR="$JANUSGRAPH_HOME/logs"
 fi
+
+export JANUSGRAPH_LOGDIR="$BIN/../log"
 
 if [[ -z "$PID_DIR" ]] ; then
-  PID_DIR="$GREMLIN_HOME/run"
+  PID_DIR="$JANUSGRAPH_HOME/run"
 fi
 
 if [[ -z "$PID_FILE" ]]; then
-  PID_FILE=$PID_DIR/gremlin.pid
+  PID_FILE=$PID_DIR/janusgraph.pid
 fi
 
-if [[ -z "$GREMLIN_YAML" ]]; then
-  GREMLIN_YAML=$GREMLIN_HOME/conf/gremlin-server.yaml
+if [[ -z "$JANUSGRAPH_YAML" ]]; then
+  JANUSGRAPH_YAML=$CFG/gremlin-server.yaml
 fi
-
-if [[ ! -r "$GREMLIN_YAML" ]]; then
-  # try relative to home
-  GREMLIN_YAML="$GREMLIN_HOME/$GREMLIN_YAML"
-  if [[ ! -r "$GREMLIN_YAML" ]]; then
-    echo WARNING: $GREMLIN_YAML is unreadable
+echo "JANUSGRAPH_YAML is $JANUSGRAPH_YAML"
+if [[ ! -r "$JANUSGRAPH_YAML" ]]; then
+  # fqdn failed, try relative to home
+  if [[ -r "$JANUSGRAPH_HOME/$JANUSGRAPH_YAML" ]]; then
+    JANUSGRAPH_YAML="$JANUSGRAPH_HOME/$JANUSGRAPH_YAML"
+  else
+    echo WARNING: Tried "$JANUSGRAPH_YAML" and "${JANUSGRAPH_HOME}/${JANUSGRAPH_YAML}". Neither were readable.
   fi
 fi
 
-# absolute file path requires 'file:'
-LOG4J_CONF="file:$GREMLIN_HOME/conf/log4j-server.properties"
-
 # Find Java
-if [[ "$JAVA_HOME" = "" ]] ; then
-    JAVA="java"
+if [ "$JAVA_HOME" = "" ] ; then
+    JAVA="java -server"
 else
-    JAVA="$JAVA_HOME/bin/java"
+    JAVA="$JAVA_HOME/bin/java -server"
 fi
 
 # Set Java options
-if [[ "$JAVA_OPTIONS" = "" ]] ; then
-    JAVA_OPTIONS="-Xms512m -Xmx4096m"
+if [ "$JAVA_OPTIONS" = "" ] ; then
+    JAVA_OPTIONS="-Xms32m -Xmx512m -javaagent:$LIB/jamm-0.3.0.jar -Dgremlin.io.kryoShimService=org.janusgraph.hadoop.serialize.JanusGraphKryoShimService"
 fi
-
-# Build Java CLASSPATH
-CP="$GREMLIN_HOME/conf/"
-CP="$CP":$( echo $GREMLIN_HOME/lib/*.jar . | sed 's/ /:/g')
-CP="$CP":$( find -L "$GREMLIN_HOME"/ext -mindepth 1 -maxdepth 1 -type d | \
-        sort | sed 's/$/\/plugin\/*/' | tr '\n' ':' )
-
-CLASSPATH="${CLASSPATH:-}:$CP"
-
-GREMLIN_SERVER_CMD=org.apache.tinkerpop.gremlin.server.GremlinServer
-GREMLIN_INSTALL_CMD=org.apache.tinkerpop.gremlin.server.util.GremlinServerInstall
-
 
 isRunning() {
   if [[ -r "$PID_FILE" ]] ; then
@@ -150,41 +161,22 @@ start() {
     exit 1
   fi
 
-  if [[ -z "$RUNAS" ]]; then
-
-    mkdir -p "$LOG_DIR" &>/dev/null
-    if [[ ! -d "$LOG_DIR" ]]; then
-      echo ERROR: LOG_DIR $LOG_DIR does not exist and could not be created.
-      exit 1
-    fi
-
-    mkdir -p "$PID_DIR" &>/dev/null
-    if [[ ! -d "$PID_DIR" ]]; then
-      echo ERROR: PID_DIR $PID_DIR does not exist and could not be created.
-      exit 1
-    fi
-
-    $JAVA -Dlog4j.configuration=$LOG4J_CONF $JAVA_OPTIONS -cp $CP:$CLASSPATH $GREMLIN_SERVER_CMD "$GREMLIN_YAML" >> "$LOG_FILE" 2>&1 &
-    PID=$!
-    disown $PID
-    echo $PID > "$PID_FILE"
-  else
-
-    su -c "mkdir -p $LOG_DIR &>/dev/null"  "$RUNAS"
-    if [[ ! -d "$LOG_DIR" ]]; then
-      echo ERROR: LOG_DIR $LOG_DIR does not exist and could not be created.
-      exit 1
-    fi
-
-    su -c "mkdir -p $PID_DIR &>/dev/null"  "$RUNAS"
-    if [[ ! -d "$PID_DIR" ]]; then
-      echo ERROR: PID_DIR $PID_DIR does not exist and could not be created.
-      exit 1
-    fi
-
-    su -c "$JAVA -Dlog4j.configuration=$LOG4J_CONF $JAVA_OPTIONS -cp $CP:$CLASSPATH $GREMLIN_SERVER_CMD \"$GREMLIN_YAML\" >> \"$LOG_FILE\" 2>&1 & echo \$! "  "$RUNAS" > "$PID_FILE"
-    chown "$RUNAS" "$PID_FILE"
+  mkdir -p "$JANUSGRAPH_LOGDIR" &>/dev/null
+  if [[ ! -d "$JANUSGRAPH_LOGDIR" ]]; then
+    echo ERROR: JANUSGRAPH_LOGDIR $JANUSGRAPH_LOGDIR does not exist and could not be created.
+    exit 1
   fi
+
+  mkdir -p "$PID_DIR" &>/dev/null
+  if [[ ! -d "$PID_DIR" ]]; then
+    echo ERROR: PID_DIR $PID_DIR does not exist and could not be created.
+    exit 1
+  fi
+
+  $JAVA -Djanusgraph.logdir="$JANUSGRAPH_LOGDIR" -Dlog4j.configuration=$LOG4J_CONF $JAVA_OPTIONS -cp $CP:$CLASSPATH $JANUSGRAPH_SERVER_CMD $JANUSGRAPH_YAML
+  PID=$!
+  disown $PID
+  echo $PID > "$PID_FILE"
 
   isRunning
   RUNNING=$?
@@ -207,7 +199,7 @@ startForeground() {
   fi
 
   if [[ -z "$RUNAS" ]]; then
-    $JAVA -Dlog4j.configuration=$LOG4J_CONF $JAVA_OPTIONS -cp $CP:$CLASSPATH $GREMLIN_SERVER_CMD "$GREMLIN_YAML"
+    $JAVA -Dlog4j.configuration=$LOG4J_CONF $JAVA_OPTIONS -cp $CP:$CLASSPATH $JANUSGRAPH_SERVER_CMD "$JANUSGRAPH_YAML"
     exit 0
   else
     echo Starting in foreground not supported with RUNAS
@@ -226,13 +218,8 @@ install() {
   fi
 
   echo Installing dependency $@
+  $JAVA -Djanusgraph.logdir="$JANUSGRAPH_LOGDIR" -Dlog4j.configuration=$LOG4J_CONF $JAVA_OPTIONS -cp $CP:$CLASSPATH org.apache.tinkerpop.gremlin.server.util.GremlinServerInstall "$@"
 
-  DEPS="$@"
-  if [[ -z "$RUNAS" ]]; then
-    $JAVA -Dlog4j.configuration=$LOG4J_CONF $JAVA_OPTIONS -cp $CP:$CLASSPATH  $GREMLIN_INSTALL_CMD $DEPS
-  else
-    su -c "$JAVA -Dlog4j.configuration=$LOG4J_CONF $JAVA_OPTIONS -cp $CP:$CLASSPATH $GREMLIN_INSTALL_CMD $DEPS "  "$RUNAS"
-  fi
 
 }
 
@@ -260,27 +247,27 @@ case "$1" in
   *)
     if [[ -n "$1" ]] ; then
       if [[ -r "$1" ]]; then
-        GREMLIN_YAML="$1"
+        JANUSGRAPH_YAML="$1"
         startForeground
-      elif [[ -r "$GREMLIN_HOME/$1" ]] ; then
-        GREMLIN_YAML="$GREMLIN_HOME/$1"
+      elif [[ -r "$JANUSGRAPH_HOME/$1" ]] ; then
+        JANUSGRAPH_YAML="$JANUSGRAPH_HOME/$1"
         startForeground
       fi
       echo Configuration file not found.
     fi
     echo "Usage: $0 {start|stop|restart|status|console|install <group> <artifact> <version>|<conf file>}"
     echo
-    echo "    start        Start the server in the background using conf/gremlin-server.yaml as the"
+    echo "    start        Start the server in the background using conf/gremlin-server/gremlin-server.yaml as the"
     echo "                 default configuration file"
     echo "    stop         Stop the server"
     echo "    restart      Stop and start the server"
     echo "    status       Check if the server is running"
-    echo "    console      Start the server in the foreground using conf/gremlin-server.yaml as the"
+    echo "    console      Start the server in the foreground using conf/gremlin-server/gremlin-server.yaml as the"
     echo "                 default configuration file"
     echo "    install      Install dependencies"
     echo
     echo "If using a custom YAML configuration file then specify it as the only argument for Gremlin"
-    echo "Server to run in the foreground or specify it via the GREMLIN_YAML environment variable."
+    echo "Server to run in the foreground or specify it via the JANUSGRAPH_YAML environment variable."
     echo
     exit 1
     ;;
